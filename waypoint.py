d3b78779737a6f1fc4fe1837e89a9b750ae38fe0
bonaime <bonaime@ipgp.fr>
2021-08-17 09:50:59 +0200

    waypoint class

--- waypoint.py
+++ waypoint.py
@@ -1,9 +1,8 @@
-import matplotlib.pyplot as plt
-from math import pi, cos, atan2, asin, acos, sin, radians, degrees
-import pandas as pd
-import plotly.graph_objects as go
+from math import atan, degrees, sqrt
+import folium
+from gpxplotter import create_folium_map
+from geopy import Point, distance
 import geopy
-import geopy.distance
 
 
 class Waypoint:
@@ -15,95 +14,146 @@ class Waypoint:
         self.altitude_aboslue_sol = None
         self.altitude_relative_drone = None
         self.hauteur_sol = None
-        self.emprise_laterale = 10  # en mètres
+        self.emprise_laterale = 100  # en mètres
         self.emprise_longitudinale = 50  # en mètres
-        self.direction = 0
+        self.direction = 30
+        self.emprise = None
+        self.name = "IPGP"
 
     def emprise_coordinates(self):
         """Détermine les coordonnées de l'emprise au sol à partir du point central
                    5 | 5
-        x0,y0 ---------------- x1,y1
+        x3,y3 ---------------- x0,y0
             |                 |
          10 |        ^        |
           – |        |        |
          10 |        X        |
             |                 |
             |                 |
-        x2,y2 ---------------- x3,y3
+        x2,y2 ---------------- x1,y1
 
         """
         delta_lat = self.emprise_laterale / 2
         delta_long = self.emprise_longitudinale / 2
 
-        x0, y0 = latitude_longitude_plus_distance(
-            self.latitude,
-            self.longitude,
-            -delta_lat * cos(radians(self.direction))
-            + delta_long * sin(radians(self.direction)),
-            delta_lat * sin(radians(self.direction))
-            + delta_long * cos(radians(self.direction)),
-        )
+        angle = degrees(atan(delta_lat/delta_long))
 
-        x1, y1 = latitude_longitude_plus_distance(
-            self.latitude,
-            self.longitude,
-            delta_lat * cos(radians(self.direction))
-            + delta_long * sin(radians(self.direction)),
-            -delta_lat * sin(radians(self.direction))
-            + delta_long * cos(radians(self.direction)),
-        )
+        dist = geopy.distance.distance(kilometers=(
+            sqrt(delta_lat * delta_lat+delta_long * delta_long)/1000))
 
-        x2, y2 = latitude_longitude_plus_distance(
-            self.latitude,
-            self.longitude,
-            delta_lat * cos(radians(self.direction))
-            - delta_long * sin(radians(self.direction)),
-            -delta_lat * sin(radians(self.direction))
-            - delta_long * cos(radians(self.direction)),
-        )
+        tmp = dist.destination(point=Point(
+            self.latitude, self.longitude), bearing=angle + self.direction)
+        #        print("angle "+str(angle + self.direction))
+        x0, y0, *_ = tmp
 
-        x3, y3 = latitude_longitude_plus_distance(
-            self.latitude,
-            self.longitude,
-            -delta_lat * cos(radians(self.direction))
-            - delta_long * sin(radians(self.direction)),
-            delta_lat * sin(radians(self.direction))
-            - delta_long * cos(radians(self.direction)),
-        )
+        tmp = dist.destination(point=Point(
+            self.latitude, self.longitude), bearing=180-angle + self.direction)
+        #print("angle "+str(180-angle + self.direction))
+        x1, y1, *_ = tmp
 
-        #        return x0, y0, x1, y1, x2, y2, x3, y3
-        data = [['Center', self.latitude, self.longitude], [
-            'X0', x0, y0], ['X1', x1, y1], ['X2', x2, y2], ['X3', x3, y3]]
-        # Create the pandas DataFrame
-        df = pd.DataFrame(data, columns=['Name', 'X', 'Y'])
-        return df
+        tmp = dist.destination(point=Point(
+            self.latitude, self.longitude), bearing=180+angle + self.direction)
+        #print("angle "+str(180+angle + self.direction))
+
+        x2, y2, *_ = tmp
+        tmp = dist.destination(point=Point(
+            self.latitude, self.longitude), bearing=360-angle + self.direction)
+
+        x3, y3, *_ = tmp
+
+        print(self.latitude, " ", self.longitude)
+        print(x0, " ", y0)
+        print(x1, " ", y1)
+        print(x2, " ", y2)
+        print(x3, " ", y3)
+
+        self.emprise = [(x0, y0), (x1, y1), (x2, y2), (x3, y3)]
+        return self.emprise
 
 
-def latitude_longitude_distance_bearing(latitude, longitude, distance, bearing):
-    print(latitude, longitude, distance_x, distance_y)
-    "Return latitude + distance_x and longitude + distance_y"
-    new_point = geopy.distance.distance(
-        kilometers=distance/1000).destination((latitude, longitude), bearing=bearing)
-    return new_point.latitude, new_point.longitude
+LAT = 48.84482270388685
+LON = 2.3562098704389163
+
+IPGP = Waypoint(LAT, LON)
+emprise_IPGP = IPGP.emprise_coordinates()
+the_map = create_folium_map(zoom_min=0, max_zoom=18, zoom_start=10)
+
+# Add custom base maps to folium
+basemaps = {
+    'Google Maps': folium.TileLayer(
+        tiles='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        attr='Google',
+        name='Google Maps',
+        overlay=False,
+        control=True
+    ),
+    'Google Satellite': folium.TileLayer(
+        tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        attr='Google',
+        name='Google Satellite',
+        overlay=True,
+        control=True
+    ),
+    'Google Terrain': folium.TileLayer(
+        tiles='https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+        attr='Google',
+        name='Google Terrain',
+        overlay=True,
+        control=True
+    ),
+    'Google Satellite Hybrid': folium.TileLayer(
+        tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+        attr='Google',
+        name='Google Satellite',
+        overlay=True,
+        control=True
+    ),
+    'Esri Satellite': folium.TileLayer(
+        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr='Esri',
+        name='Esri Satellite',
+        overlay=True,
+        control=True
+    )
+}
+
+# Add custom basemaps
+basemaps['Google Maps'].add_to(the_map)
+basemaps['Google Satellite'].add_to(the_map)
+basemaps['Google Terrain'].add_to(the_map)
+basemaps['Google Satellite Hybrid'].add_to(the_map)
+basemaps['Esri Satellite'].add_to(the_map)
+
+tiles_maps = ['openstreetmap', 'Stamen Terrain']
+#tiles_maps = ['openstreetmap']
+#tiles_maps = []
+# tiles_maps=[ 'openstreetmap',''Cartodb Positron',
+# 'Stamen Terrain','Stamen Toner','Stamen Watercolor']
+
+for tile in tiles_maps:
+    folium.TileLayer(tile).add_to(the_map)
+folium.Marker(
+    location=[LAT, LON],
+    # popup=image.filename,
+    # color=color_image,
+    #                icon=folium.Icon(color=color_image,icon='fas fa-camera')
+    icon=folium.Icon(color='blue', icon='fa-map-pin')).add_to(the_map)
+
+folium.Marker(location=emprise_IPGP[0], popup='X0', icon=folium.Icon(
+    color='red', icon='fa-map-pin')).add_to(the_map)
+folium.Marker(location=emprise_IPGP[1], popup='X1', icon=folium.Icon(
+    color='green', icon='fa-map-pin')).add_to(the_map)
+folium.Marker(location=emprise_IPGP[2], popup='X2', icon=folium.Icon(
+    color='green', icon='fa-map-pin')).add_to(the_map)
+folium.Marker(location=emprise_IPGP[3], popup='X3', icon=folium.Icon(
+    color='red', icon='fa-map-pin')).add_to(the_map)
+
+the_map.add_child(folium.vector_layers.Polygon(locations=emprise_IPGP, color='#ff7800', fill=True, fill_color='#ffff00', fill_opacity=0.2,
+                                               weight=2, popup=(folium.Popup(IPGP.name))))
+
+boundary = the_map.get_bounds()
+the_map.fit_bounds(boundary, padding=(5, 5))
+folium.LayerControl(sortLayers=True).add_to(the_map)
 
 
-IPGP = Waypoint(2.3562098704389163, 48.84482270388685)
-# IPGP = Waypoint(0, 0)
-df = IPGP.emprise_coordinates()
-print(df)
-fig = go.Figure(data=go.Scattergeo(
-    lon=df['X'],
-    lat=df['Y'],
-    text=df['Name'],
-    mode='markers'
-))
-
-fig.update_layout(
-    title='',
-    geo_scope='europe',
-)
-fig.show()
-
-
-IPGP.direction = 90
-df = IPGP.emprise_coordinates
+the_map.save('./carte.html')
