8451a87820f4b984b3454687219aa40a00bc87a3
bonaime <bonaime@ipgp.fr>
2021-08-30 14:19:18 +0200

    style dans pathplanning

--- pathplanning.py
+++ pathplanning.py
@@ -8,19 +8,23 @@ from geopy.distance import geodesic
 class PathPlanning:
     "PathPlanning class"
 
-    def __init__(self, AOI, emprise_laterale, emprise_longitudinale):
+    def __init__(self, A,B, emprise_laterale, emprise_longitudinale):
         """
-        X0, Y0 ----------------- X1, Y1
+        A(X0, Y0) ----------------- X1, Y0
         |                        |
         |                        |
         |                        |
         |                        |
         |                        |
         |                        |
-        X0, Y1 ----------------- X1, Y1
+        X0, Y1 ----------------- B(X1, Y1)
         """
 
-        self.AOI = AOI
+       #self.AOI = [(X0, Y0),  (X0, Y1), (X1, Y1), (X1, Y0)]
+        self.A=A
+        self.B=B
+        self.AOI = [(A[0], A[1]),  (A[0], B[1]), (B[0], B[1]), (B[1], A[1])]
+
         self.best_orientation = None
 
         self.emprise_laterale = emprise_laterale  # en mètres
@@ -46,21 +50,40 @@ class PathPlanning:
             self.best_orientation = 90
 
         print("Best Orientation "+str(self.best_orientation))
+    
+    def GeneratePath(self,style):
+        if style == "snail":
+            self.GeneratePathSnail()
+        elif style == "normal":
+            self.GeneratePath()
+
+    def GeneratePathSnail(self):
+        pass
+    
+    def GeneratePathNormal(self):
+        pass
 
 
+
+
+    
 def main(args):
 
     LAT = 48.84482270388685
     LON = 2.3562098704389163
 
-    X0 = 48.84643250706535
-    Y0 = 2.3527444567404943
-    X1 = 48.84350234422499
-    Y1 = 2.358956452357875
-    AOI = [(X0, Y0),  (X0, Y1), (X1, Y1), (X1, Y0)]
+#    X0 = 48.84643250706535
+#    Y0 = 2.3527444567404943
 
-    pp = PathPlanning(AOI, 100, 50)
+    A = (48.84643250706535,2.3527444567404943)
+    B = (48.84350234422499,2.358956452357875 )
+#    X1 = 48.84350234422499
+#    Y1 = 2.358956452357875
+
+    pp = PathPlanning(A,B, 100, 50)
     pp.find_best_orientation()
+    pp.GeneratePath("snail")
+#    pp.GeneratePath("snail")
 
 
 if __name__ == '__main__':
