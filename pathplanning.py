b1bcb518ee549dd34e32d97f743d193a14827f5e
bonaime <bonaime@ipgp.fr>
2021-08-30 18:03:00 +0200

    outil angle

--- pathplanning.py
+++ pathplanning.py
@@ -3,54 +3,40 @@ from geopy import Point, distance
 import geopy
 import sys
 from geopy.distance import geodesic
+import pyproj
+from math import degrees, acos
 
 
 class PathPlanning:
     "PathPlanning class"
 
-    def __init__(self, A,B, emprise_laterale, emprise_longitudinale):
+    def __init__(self, A,B,C,D ,emprise_laterale, emprise_longitudinale):
         """
-        A(X0, Y0) ----------------- X1, Y0
+        A(Xa, Ya) ----------------- B(Xb, Yb)
         |                        |
         |                        |
         |                        |
         |                        |
         |                        |
         |                        |
-        X0, Y1 ----------------- B(X1, Y1)
+        D(Xd, Yd) ----------------- C(Xc, Yc)
         """
+        #c^2=a^2+b^2-2*ab*cos(C)
 
        #self.AOI = [(X0, Y0),  (X0, Y1), (X1, Y1), (X1, Y0)]
         self.A=A
         self.B=B
-        self.AOI = [(A[0], A[1]),  (A[0], B[1]), (B[0], B[1]), (B[1], A[1])]
+        self.C=C
+        self.D=D
 
         self.best_orientation = None
 
         self.emprise_laterale = emprise_laterale  # en mètres
         self.emprise_longitudinale = emprise_longitudinale  # en mètres
 
-    def create_back_foth_route(self):
-        pass
+       # vertical_distance = geodesic(self.AOI[0], self.AOI[1]).km
+       # horizontal_distance = geodesic(self.AOI[0], self.AOI[2]).km
 
-    def find_best_orientation(self):
-        # X1-X0 ou Y1-Y0 ?
-
-        # Print the distance calculated in km
-        #print(geodesic(self.AOI[0], self.AOI[1]).km)
-        vertical_distance = geodesic(self.AOI[0], self.AOI[1]).km
-        horizontal_distance = geodesic(self.AOI[0], self.AOI[2]).km
-
-        print("Vertical "+str(vertical_distance))
-        print("Horizontal "+str(horizontal_distance))
-
-        if(vertical_distance > horizontal_distance):
-            self.best_orientation = 0
-        else:
-            self.best_orientation = 90
-
-        print("Best Orientation "+str(self.best_orientation))
-    
     def GeneratePath(self,style):
         if style == "snail":
             self.GeneratePathSnail()
@@ -64,6 +50,13 @@ class PathPlanning:
         pass
 
 
+def getAngle(A,B,C):
+    "return angle between A,B and C in degrees"
+    AB=geodesic(A,B).m
+    BC=geodesic(B,C).m
+    CA=geodesic(A,C).m
+    return degrees(acos((AB*AB+BC*BC-CA*CA)/(2*AB*BC)))
+        
 
 
     
@@ -75,10 +68,26 @@ def main(args):
 #    X0 = 48.84643250706535
 #    Y0 = 2.3527444567404943
 
-    A = (48.84643250706535,2.3527444567404943)
-    B = (48.84350234422499,2.358956452357875 )
+    A = (48.844781966005414, 2.354806246580006)
+    B = (48.845476490908986, 2.3559582742434224)
+    C = (48.844800522139515, 2.356945151087957)
+    D = (48.84395753653702, 2.355015706155173)
 #    X1 = 48.84350234422499
-#    Y1 = 2.358956452357875
+
+
+    # Calcul des distances
+    AB=geodesic(A,B).m
+    BC=geodesic(B,C).m
+    CD=geodesic(C,D).m
+    DA=geodesic(D,A).m
+    BD=geodesic(B,D).m
+    AC=geodesic(A,C).m
+
+    # Calcul des angles
+    angle_ABC=getAngle(A,B,C)
+    angle_BCD=getAngle(B,C,D)
+    angle_CDA=getAngle(C,D,A)
+    angle_DAB=getAngle(D,A,B)
 
     pp = PathPlanning(A,B, 100, 50)
     pp.find_best_orientation()
