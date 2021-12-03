ec990400ad7e071cce8bcf83809c97bdf49149e7
scpsc <arthur.github@gmail.com>
2021-12-03 16:44:12 +0100

    another test data

--- drone_orientation/README.md
+++ drone_orientation/README.md
@@ -4,7 +4,10 @@ Defines the successive positions (orientations) of the drone along one path, to 
 In addition, the gimbal pitch is adapted, so that photos are taken parallel to the ground.
 
 ![prof1](/drone_orientation/prof1_orientations.svg)
+Pitch angle is fixed
+
 ![prof2](/drone_orientation/prof2_orientations.svg)
+Pitch angle is estimated for each orientation
 
 ## TODO
 
--- drone_orientation/droneorientation_test.py
+++ drone_orientation/droneorientation_test.py
@@ -9,16 +9,19 @@ from classes.droneorientation import DroneOri
 # This script is to test droneorientation.py
 # Test data are RGE ALTI data, from IGN (https://geoservices.ign.fr/rgealti)
 
+# Example1: fixed pitch, with the drone shooting backwards
 np_dsm = np.array(ImagePil.open('rge_alti_1m_1.tif'))
 prof1 = DroneOri(
         name='prof1', np_dsm=np_dsm, tfw='rge_alti_1m_1.tfw',
         a_east=365649.5, a_north=6397299.5, b_east=366205.5, b_north=6396986.5,
-        h=20, sensor_size=(23.5,15.7), img_size=(6016,3376), focal=24, ovlp=0.1
+        h=20, sensor_size=(23.5,15.7), img_size=(6016,3376), focal=24, ovlp=0.1,
+        fixed_pitch=75
     )
 prof1.dsm_profile()
 prof1.drone_orientations()
 prof1.draw_orientations(disp_linereg=True, disp_footp=True, disp_fov=True)
 
+# Example2: the pitch angle is estimated for each orientation
 np_dsm = np.array(ImagePil.open('rge_alti_1m_2.tif'))
 prof2 = DroneOri(
         name='prof2', np_dsm=np_dsm, tfw='rge_alti_1m_2.tfw',
