fd89eb9e2e5a12ef3962ea57e2f8e33a0b82c6f4
bonaime <bonaime@ipgp.fr>
2022-03-30 13:13:17 +0200

    google_kml_to_coordinates.py

--- google_kml_to_coordinates.py
+++ google_kml_to_coordinates.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser(description='Process Google Kml to coordinates for .ini file')
+parser.add_argument('--infile', '-i',type=argparse.FileType('r', encoding='UTF-8'), 
+                      required=True)
+args = parser.parse_args()
 
 debug = False
-
-inputname = './Noisiel Mapping.kml'
-
 next_line_is_coordinates = False
 
-# Read KML file and get coordinates
-with open(inputname) as kml_file:
-     while line := kml_file.readline():
-        #print(line.rstrip())        
-        if "<coordinates>" in line:
-            next_line_is_coordinates = True
-        
-        if next_line_is_coordinates:
-            line = kml_file.readline()
-            coordinates = line
-            next_line_is_coordinates = False
+while line := args.infile.readline():
+    if "<coordinates>" in line:
+        next_line_is_coordinates = True
+
+    if next_line_is_coordinates:
+        line = args.infile.readline()
+        coordinates = line
+        next_line_is_coordinates = False
 
 coordinates_string=coordinates.lstrip().rstrip()
 
@@ -27,7 +27,6 @@ for coordinate in coordinates_string.split(" "):
     final_string += f'({lat},{lon}),'
 
 # end of coordinates
-
 # strip last coma
 final_string = final_string[:-1]
 # add parenthesis
