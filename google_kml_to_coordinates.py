4c9d323f7fbefbefe44cd6e31caa7df6ecc6c23c
bonaime <bonaime@ipgp.fr>
2022-03-30 14:35:17 +0200

    coordinates in place

--- google_kml_to_coordinates.py
+++ google_kml_to_coordinates.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 import argparse
+from genericpath import exists
+from tempfile import mkstemp
+from shutil import move, copymode
+import os 
+
+
 
 parser = argparse.ArgumentParser(description='Process Google Kml to coordinates for .ini file')
 parser.add_argument('--infile', '-i',type=argparse.FileType('r', encoding='UTF-8'), 
                       required=True)
+parser.add_argument('--out_ini_file', '-o', required=False)
+
 args = parser.parse_args()
 
 debug = False
@@ -36,6 +44,22 @@ for  i in range(nb_of_coordinates-1):
 # strip last coma
 final_string = final_string[:-1]
 # add parenthesis
-final_string += ']'
+final_string += ']\n'
 
 print(final_string)
+
+# Write to .ini file
+if args.out_ini_file:
+    # Creating a temp file
+    fd, abspath = mkstemp()
+    with os.fdopen(fd,'w') as tmp_file:
+        with open( args.out_ini_file,'r') as file0:
+            for line in file0:
+                if not "points_list =[" in line:
+                    tmp_file.write(line)
+                else :
+                   tmp_file.write(final_string)
+    
+    copymode(args.out_ini_file, abspath)
+    os.remove(args.out_ini_file)
+    move(abspath, args.out_ini_file)
